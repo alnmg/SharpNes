2ff1db6ca6d276dd27ef9aaace1980a490a13b1f
alnmg <alnmgdev@gmail.com>
2023-03-06 18:37:55 -0300

    first commit

---
+++ src/NES/NES.cs
@@ -0,0 +1,6 @@
+namespace SharpNes.NES;
+
+public class NES
+{
+    //construtor que herde tudo vai aqui, aqui tambem iram classes gerais que controlam acesso entre componentes
+}
---
+++ src/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace SharpNes.NES;
+
+public class main{
+    public static void Main(){
+        System.Console.WriteLine("o chupa cu esta vivo");
+    }
+}
